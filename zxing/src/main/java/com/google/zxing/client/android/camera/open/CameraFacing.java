e18cf70245e72c30accdd4771da02440ff2c73f3
kc <qiqi1995822@163.com>
2018-02-02 10:30:21 +0800

    Initial commit

---
+++ zxing/src/main/java/com/google/zxing/client/android/camera/open/CameraFacing.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (C) 2015 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 枚举变量 值为0代表后置摄像头、1代表前置摄像头
+ */
+
+package com.google.zxing.client.android.camera.open;
+
+/**
+ * Enumeration of directions a camera may face: front or back.
+ */
+public enum CameraFacing {
+
+  BACK,  // must be value 0!
+  FRONT, // must be value 1!
+
+}
